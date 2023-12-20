16fc95be85b90b0446961d6b0da4d2a7272f22c7
tzlilashkenazi <t.ashkenazi@emergecds.com>
2023-12-20 21:47:16 +0200

    remove text

--- src/const.tsx
+++ src/const.tsx
@@ -1,4 +1,4 @@
-export const  TIME_PER_QUESTION:number = 2
-export const  TIME_FOR_HINT:number = TIME_PER_QUESTION - 1
+export const  TIME_PER_QUESTION:number = 20
+export const  TIME_FOR_HINT:number = TIME_PER_QUESTION - 10
 
 
